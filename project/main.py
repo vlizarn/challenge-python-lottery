b626e01345c416ce9c4c875f5e33fb209202b57d
vlizarn <94148848+vlizarn@users.noreply.github.com>
2021-12-04 23:56:05 +0000

    Add the main.py file with the Program class.

--- project/main.py
+++ project/main.py
@@ -1,1 +1,40 @@
+import os
+import sys
 
+class Program:
+
+    def __init__(self):
+        self.run = 1
+        self.main()
+
+    def __del__(self):
+        print("\nMessage: The program closed!")
+
+    def status(self):
+        step = input("\nDo you want close program (Yes or No)?\n")
+
+        clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+        if(step == "Yes"):
+            self.run = 0
+        elif(step == "No"):
+            clear()
+        else:
+            print("\nMessage: Try another option!")
+            self.status()
+
+    def main(self):
+
+        while self.run == 1:
+
+            try:
+                pass
+
+            except:
+                pass
+                
+            finally:
+                self.status()
+
+app = Program()
+app.main()
