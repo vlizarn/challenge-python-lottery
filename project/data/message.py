e6d52724db4af0906dde4a00fd8e6e8bef1950d3
vlizarn <94148848+vlizarn@users.noreply.github.com>
2021-12-07 23:57:31 +0000

    Add message file that contains the tagDict and reportDict.

---
+++ project/data/message.py
@@ -0,0 +1,32 @@
+tagDict = {
+    "null": "",
+    "msg": "\nMessage: ",
+}
+
+reportDict = {
+    "null": [
+        "",
+    ],
+    "action": [
+        "The program closed!\n",
+        "Try another option!",
+    ],
+    "decision": [
+        "Yes",
+        "No",
+    ],
+    "lottery": [
+        "You won the lottery!",
+        "You don't win the lottery!",
+        "The number of bets is fewer than permitted!",
+    ],
+    "error": [
+        "\nUnexpected error: ",
+        "Error detect, Try another option!",
+    ],
+    "request": [
+        "\nInsert a bet mode (Simple or Multiple): ",
+        "Insert a amount of bet: ",
+        "\nDo you want close program (Yes or No)?\n",
+    ],
+}
